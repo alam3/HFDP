f59669fff4436647c89f15ac691816bd72c7e37a
Andrew Lam <andrewlamf@gmail.com>
2022-05-19 14:56:20 -0400

    Chapter 03 - Testing decorator pattern to build coffee beverages

--- Chapter03/StarBuzz/Program.cs
+++ Chapter03/StarBuzz/Program.cs
@@ -1,9 +1,24 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace StarBuzz {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            // Building beverages using the Decorator Pattern
+
+            Beverage beverage = new Espresso();
+            WriteLine(beverage.GetDescription() + " $" + beverage.Cost());
+
+            Beverage beverage2 = new DarkRoast();
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Mocha(beverage2); // Adding a second Mocha shot
+            beverage2 = new Whip(beverage2);
+            WriteLine(beverage2.GetDescription() + " $" + beverage2.Cost());
+
+            Beverage beverage3 = new HouseBlend();
+            beverage3 = new Soy(beverage3);
+            beverage3 = new Mocha(beverage3);
+            beverage3 = new Whip(beverage3);
+            WriteLine(beverage3.GetDescription() + " $" + beverage3.Cost());
         }
     }
 }
