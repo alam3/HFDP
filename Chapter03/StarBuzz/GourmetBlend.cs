752a506d48429a624b6eb043b5957e1cc09171d3
Andrew Lam <lekz01@gmail.com>
2022-05-19 17:23:14 -0400

    Chapter 03 - Additional base beverage concrete implementation

---
+++ Chapter03/StarBuzz/GourmetBlend.cs
@@ -0,0 +1,9 @@
+public class GourmetBlend : Beverage {
+    public GourmetBlend() {
+        description = "Gourmet Blend Coffee";
+    }
+
+    public override double Cost() {
+        return 2.89;
+    }
+}
